82aa8e0818116d654d2950303a63ee8704ed4846
Rajibul Islam <rajibulski110@gmail.com>
2025-01-07 20:58:32 +0530

    added navigation component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>The Wild Oasis. Welcome to Paradise.</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to Paradise.</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
